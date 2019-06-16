657419688de30e415767ac78b6cddc5399a0eeed
Daniel Favano <dfavano@breg.com>
2019-06-15 21:48:14 -0700

    Meta-data of my favorite song.

---
+++ FavoriteSong.playground/Contents.swift
@@ -0,0 +1,55 @@
+import UIKit
+
+/*
+ Author: Daniel Favano
+ Decription: This is part of the Swift course being offered by pirple.com. The cource is broken up over 3 parts and this is part 1 of the class.
+ It revolved mostly around the core syntax of Swift. The first assignment is create a playground that outlines the meta-data of my favorite song.
+ The assignment is designed to get students accustmed to using variables and there various types. This is way this is not represented in something
+ like a class or JSON.
+ Homework Assignment: 1 (Favorite Song)
+*/
+
+let title : String = "When the Levee Breaks"
+let writtenBy : String = "Kansas Joe McCoy and Memphis Minnie"
+let originallyReleased = (month: 6, day: 18, year: 1929)
+let coveredBy : String = "Led Zepplin"
+let ledZepplinBandMembers = (singer: "Robert Plant", guitar: "Jimmmy Page", drums: "John Bonham", bass: "John Paul Jones")
+let coveredOn = (month: 11, day: 8, year: 1971)
+let ledZepplinAlbum : Int = 4
+let length : Decimal = 7.08
+let genre : String = "Hard Rock, Blues"
+let link : String = "https://www.youtube.com/watch?v=b97hqSDRspw"
+let about : String = "https://en.wikipedia.org/wiki/Great_Mississippi_Flood_of_1927"
+let lyrics : String = """
+If it keeps on rainin', levee's goin' to break
+If it keeps on rainin', levee's goin' to break
+When the levee breaks I'll have no place to stay
+Mean old levee taught me to weep and moan
+Lord mean old levee taught me to weep and moan
+It's got what it takes to make a mountain man leave his home
+Oh well, oh well, oh well
+Don't it make you feel bad
+When you're tryin' to find your way home
+You don't know which way to go?
+If you're goin' down South
+They got no work to do
+If you don't know about Chicago
+Cryin' won't help you prayin' won't do you no good
+Now cryin' won't help you prayin' won't do you no good
+When the levee breaks mama you got to move
+All last night sat on the levee and moaned
+All last night sat on the levee and moaned
+Thinkin' 'bout me baby and my happy home
+Going to Chicago
+Going to Chicago
+Sorry but I can't take you
+Going down, going down now, going down
+Going down now, going down
+Going down, going down, going down
+Going down now, going down
+Going down now, going down
+Going down now, going down
+Going d-d-d-d-down
+Woo, woo
+"""
+
